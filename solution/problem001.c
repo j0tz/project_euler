efc6e92c8ae99f738269f755f8496cd7db1f4926
João Paulo <joaopaulo4911@gmail.com>
2022-11-23 00:53:11 -0300

    Whitespace characters removed

--- solution/problem001.c
+++ solution/problem001.c
@@ -6,17 +6,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() 
+int main()
 {
 	int result = 0, multiple_three, multiple_five;
 
-	for (multiple_three = 0; multiple_three * 3 < 1000; multiple_three++) 
+	for (multiple_three = 0; multiple_three * 3 < 1000; multiple_three++)
 	{
 		if (multiple_three % 5 != 0)
 			result += multiple_three * 3;
 	}
 
-	for (multiple_five = 0; multiple_five * 5 < 1000; multiple_five++) 
+	for (multiple_five = 0; multiple_five * 5 < 1000; multiple_five++)
 	{
 		result += multiple_five * 5;
 	}
